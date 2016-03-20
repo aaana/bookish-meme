639be6b40a54ef45cc02920754f460d55e26bf01
谭靖儒 <120165967@qq.com>
2016-03-20 14:36:18 +0800

    实现操作流水化，添加channel管理模块

--- src/main/java/server/ChatServerHandler.java
+++ src/main/java/server/ChatServerHandler.java
@@ -1,5 +1,6 @@
 package server;
 
+import channel.Manager;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundMessageHandlerAdapter;
@@ -12,30 +13,29 @@ import java.util.function.IntConsumer;
  * Created by tanjingru on 3/17/16.
  */
 public class ChatServerHandler extends ChannelInboundMessageHandlerAdapter<String> {
-    private static final ChannelGroup channels = new DefaultChannelGroup();
 
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
         Channel incoming  = ctx.channel();
-        for ( Channel channel : channels){
+        for ( Channel channel : Manager.channels){
             channel.write("[SERVER] - " + incoming.remoteAddress() + "has joined\n");
         }
-        channels.add(ctx.channel());
+        Manager.channels.add(ctx.channel());
     }
 
     @Override
     public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
         Channel incoming = ctx.channel();
-        for ( Channel channel : channels){
+        for ( Channel channel : Manager.channels){
             channel.write("[SERVER] - " + incoming.remoteAddress() + "has left\n");
         }
-        channels.remove(ctx.channel());
+        Manager.channels.remove(ctx.channel());
     }
 
     @Override
     public void messageReceived(ChannelHandlerContext channelHandlerContext, String message) throws Exception {
         Channel incoming  = channelHandlerContext.channel();
-        for (Channel channel : channels){
+        for (Channel channel : Manager.channels){
             if ( channel != incoming){
                 channel.write("[" + incoming.remoteAddress() + "]" + message + "\n");
             }
