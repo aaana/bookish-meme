c73eb20cd803afb4e08944bfe921abf693e94514
maerye <350046243@qq.com>
2016-03-21 22:04:50 +0800

    configurable limiter params

--- src/main/java/server/Limiter.java
+++ src/main/java/server/Limiter.java
@@ -1,19 +1,27 @@
 package server;
 
+import Util.Conf;
+import Util.ConfigReader;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundMessageHandlerAdapter;
 import message.Message;
 
-import java.util.HashMap;
 import com.google.common.util.concurrent.RateLimiter;
 /**
  * Created by 马二爷 on 2016/3/20.
  */
 public class Limiter extends ChannelInboundMessageHandlerAdapter<Message> {
     private int receivedNumber=0;
-    private static final Integer maxMsgNumber=100;// configurable
-    private static final Integer maxMsgNumberPerSec=5;//configurable
-    final RateLimiter rateLimiter=RateLimiter.create(maxMsgNumberPerSec);
+    private int maxMsgNumber;// configurable
+    private int maxMsgNumberPerSec;//configurable
+    private final RateLimiter rateLimiter;
+    public Limiter() throws Exception {
+        ConfigReader reader = new ConfigReader("conf.json");
+        Conf conf = reader.readConf();
+        maxMsgNumber=conf.getMaxMsgNumber();
+        maxMsgNumberPerSec=conf.getMaxMsgNumberPerSec();
+        rateLimiter=RateLimiter.create(maxMsgNumberPerSec);
+    }
     @Override
     public void messageReceived(ChannelHandlerContext ctx, Message msg) throws Exception {
         if(rateLimiter.tryAcquire())
