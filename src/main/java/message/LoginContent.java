7e00fac897d2b169be20723f68c93adda43f71c8
Anna <593880978@qq.com>
2016-03-21 20:34:07 +0800

    partial authority

--- src/main/java/message/LoginContent.java
+++ src/main/java/message/LoginContent.java
@@ -6,4 +6,21 @@ package message;
 public class LoginContent {
     private String account;
     private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
 }
---
+++ src/main/java/server/AuthorityHandler.java
@@ -0,0 +1,7 @@
+package server;
+
+/**
+ * Created by huanganna on 16/3/21.
+ */
+public class AuthorityHandler {
+}
