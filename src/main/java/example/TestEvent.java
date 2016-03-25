05cd9cb40dd47130b91754732b4b4bbd6d8fca38
谭靖儒 <120165967@qq.com>
2016-03-25 14:14:40 +0800

    时间机制示例

---
+++ src/main/java/example/TestEvent.java
@@ -0,0 +1,15 @@
+package example;
+
+/**
+ * Created by tanjingru on 3/25/16.
+ */
+public class TestEvent {
+    private final int message;
+    public TestEvent(int message) {
+        this.message = message;
+        System.out.println("event message:"+message);
+    }
+    public int getMessage() {
+        return message;
+    }
+}
