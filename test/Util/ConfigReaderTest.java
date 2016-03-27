90bc8c14ccd99425b6fe662e579ae7808e26fabd
Anna <593880978@qq.com>
2016-03-27 19:04:20 +0800

    单元测试配置模块

---
+++ test/Util/ConfigReaderTest.java
@@ -0,0 +1,41 @@
+package Util;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by huanganna on 16/3/27.
+ */
+public class ConfigReaderTest {
+    private ConfigReader reader;
+    private String host;
+    private int port;
+    private int maxMsgNumber;
+    private int maxMsgNumberPerSec;
+
+    @Before
+    public void setUp() throws Exception {
+        reader = new ConfigReader();
+
+    }
+
+    @Test
+    public void testReadConf() throws Exception {
+        Conf conf = reader.readConf("conf.json");
+        host = conf.getHost();
+        port = conf.getPort();
+        maxMsgNumber = conf.getMaxMsgNumber();
+        maxMsgNumberPerSec = conf.getMaxMsgNumberPerSec();
+
+
+        assertEquals(host,"localhost");
+        assertEquals(port,8080);
+        assertEquals(maxMsgNumber,100);
+        assertEquals(maxMsgNumberPerSec,5);
+
+    }
+}
