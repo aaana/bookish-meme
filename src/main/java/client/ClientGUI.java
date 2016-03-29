b164d738cb84c122ee537894579c980dad4db549
lyn <tongji_lyn@163.com>
2016-03-29 19:59:37 +0800

    发送过多消息重新登录
    rt

--- src/main/java/client/ClientGUI.java
+++ src/main/java/client/ClientGUI.java
@@ -3,6 +3,7 @@ package client;
 import com.google.common.eventbus.Subscribe;
 import event.LoginFailEvent;
 import event.LoginSuccessEvent;
+import event.ReLoginEvent;
 import event.ReceiveMessageEvent;
 import javafx.concurrent.Service;
 import javafx.stage.WindowEvent;
@@ -347,6 +348,21 @@ public class ClientGUI extends Application {
         });
     }
 
+    @Subscribe
+    public void ReLogin(ReLoginEvent event) {
+        System.out.println("ReLogin");
+        Platform.runLater(new Runnable(){
+            @Override public void run() {
+                final Alert alert = new Alert(Alert.AlertType.INFORMATION); // 實體化Alert對話框物件，並直接在建構子設定對話框的訊息類型
+                alert.setTitle("出错提示"); //設定對話框視窗的標題列文字
+                alert.setHeaderText("请重新登录"); //設定對話框視窗裡的標頭文字。若設為空字串，則表示無標頭
+                alert.setContentText("您发送的消息已经达到上线,请重新登录。"); //設定對話框的訊息文字
+                alert.showAndWait(); //顯示對話框，並等待對話框被關閉時才繼續執行之後的程式
+                updateScene(sceneLogin);
+                backLogin();
+            }
+        });
+    }
 
     @Subscribe
     public void receiveOtherMessage(ReceiveMessageEvent event){
