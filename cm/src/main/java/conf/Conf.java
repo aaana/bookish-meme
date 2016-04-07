227b6c7b3cb92fa0704fcad38cd8d3f7e374b640
Anna <593880978@qq.com>
2016-04-07 23:01:18 +0800

    CM module添加抛出异常机制

--- cm/src/main/java/conf/Conf.java
+++ cm/src/main/java/conf/Conf.java
@@ -3,9 +3,14 @@ package conf;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import exception.FileNotExistException;
+import exception.KeyNotExistException;
+import exception.NoConfigurationFileException;
+import exception.TypeConvertionException;
 import org.apache.commons.io.IOUtils;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 
 /**
@@ -17,11 +22,16 @@ public class Conf {
     public void readFile(String confAddr) throws Exception{
         JsonParser parser = new JsonParser();
         Gson gson = new Gson();
-        FileInputStream configIn = new FileInputStream(confAddr);
-        jsonObject = parser.parse(IOUtils.toString(configIn)).getAsJsonObject();
+        FileInputStream configIn;
+        try {
+            configIn = new FileInputStream(confAddr);
+            jsonObject = parser.parse(IOUtils.toString(configIn)).getAsJsonObject();
+        } catch (FileNotFoundException e) {
+            throw new FileNotExistException();
+        }
     }
 
-    public Conf(JsonObject jsonObject) {
+    private Conf(JsonObject jsonObject) {
         this.jsonObject = jsonObject;
     }
 
@@ -29,20 +39,56 @@ public class Conf {
 
     }
 
-    public int getInt(String key){
-        return jsonObject.get(key).getAsInt();
+    private boolean readFileOrNot(){
+        if(jsonObject==null)
+            return false;
+        return true;
     }
 
-    public String getString(String key){
-        return jsonObject.get(key).getAsString();
+    public int getInt(String key)throws Exception{
+        if(!readFileOrNot()){
+            throw new NoConfigurationFileException();
+        }
+        if(jsonObject.get(key)==null){
+            throw new KeyNotExistException();
+        }
+        try{
+            int result = jsonObject.get(key).getAsInt();
+            return result;
+        }catch (NumberFormatException e){
+            throw new TypeConvertionException();
+        }
+
     }
 
-    public Conf getConf(String key){
+    public String getString(String key)throws Exception{
+        if(!readFileOrNot()){
+            throw new NoConfigurationFileException();
+        }
+        if(jsonObject.get(key)==null){
+            throw new KeyNotExistException();
+        }
+        try{
+            String result = jsonObject.get(key).getAsString();
+            return result;
+        }catch (NumberFormatException e){
+            throw new TypeConvertionException();
+        }
+
+    }
+
+    public Conf getConf(String key) throws Exception{
+        if(!readFileOrNot()){
+            throw new NoConfigurationFileException();
+        }
         JsonObject jsonObjectNew = jsonObject.getAsJsonObject(key);
         return new Conf(jsonObjectNew);
     }
 
-    public <T> T toObj(Class<T> t){
+    public <T> T toObj(Class<T> t) throws Exception{
+        if(!readFileOrNot()){
+            throw new NoConfigurationFileException();
+        }
         Gson gson = new Gson();
         T result = gson.fromJson(jsonObject.toString(), t);
         return result;
