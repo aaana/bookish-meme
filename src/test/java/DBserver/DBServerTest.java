be011fd36277b9e4796f9bf3ad2baf0b4bf8c396
Anna <593880978@qq.com>
2016-06-01 19:13:37 +0800

    更新1. 项目文档 2. 项目管理文档 3. 测试文档 4. 测试文档

--- src/test/java/DBserver/DBServerTest.java
+++ src/test/java/DBserver/DBServerTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -14,15 +15,33 @@ import static org.junit.Assert.*;
  */
 public class DBServerTest {
 
-    Config config = new Config();
+    private Config config = new Config();
+    private DBServer dbServer;
+
+    @Before
+
+    public void setUp() throws Exception{
+        config.readFile("config/conf.json");
+        Registry reg = LocateRegistry.getRegistry("localhost", 2017);
+        dbServer = (DBServer)(reg.lookup("DBServer"));
+    }
 
     @Test
     public void testGetGidByAcc() throws Exception {
 
-        config.readFile("config/conf.json");
-        Registry reg = LocateRegistry.getRegistry("localhost", 2017);
-        DBServer dbServer = (DBServer)(reg.lookup("DBServer"));
-
         assertEquals(dbServer.getGidByAcc(100),1);
+    }
+
+    @Test
+    public void testGetGidAndAcc() throws Exception{
+        Map<String,Integer> uidAndGids = dbServer.getGidAndUid();
+        for(String accountId : uidAndGids.keySet()){
+            System.out.println("账号："+accountId+"\t"+"组号："+uidAndGids.get(accountId));
+        }
+    }
+
+    @Test
+    public void testgetALLMessageByGid() throws Exception{
+        dbServer.getALLMessageByGid(1);
     }
 }
